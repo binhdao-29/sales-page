1ee9a5f728c43dcacb19be3029cd17986cb233f9
binhdao29 <binhchan0293@gmail.com>
2021-03-16 10:52:57 +0700

    create Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+export default function () {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Products</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Cart</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Users
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Signed in
+                </DropdownItem>
+                <DropdownItem>
+                  Signed up
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Help</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}

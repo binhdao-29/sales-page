aff042b5d9646b8fba9c68422b24c4023323e157
binhdao29 <binhchan0293@gmail.com>
2021-03-18 12:49:12 +0700

    add slide show in home component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from './components/Navbar';
+import Advertisement from './components/home/Advertisement';
 import Home from './components/Home';
 import Product from './components/Product';
 import SignUp from './components/login/SignUp';
 import SignIn from './components/login/SignIn';
-
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Advertisement} />
         <Route path="/products/" exact component={Product} />
         <Route path="/login" exact component={SignIn} />
         <Route path="/signup" exact component={SignUp} />

707c9e72a729c3693a0e7f680b9887467910424b
binhdao29 <binhchan0293@gmail.com>
2021-03-19 09:54:53 +0700

    complete advertisment component

--- src/components/home/Advertisement.js
+++ src/components/home/Advertisement.js
@@ -11,21 +11,18 @@ import {
 const items = [
   {
     id: 1,
-    src: "https://loremflickr.com/800/200/food",
-    altText: 'Slide 1',
-    caption: 'Slide 1'
+    src: "https://cf.shopee.vn/file/a3c7a91512dc7d083a4aaa989656895e_xxhdpi",
+    altText: 'Slide 1'
   },
   {
     id: 2,
-    src: "https://loremflickr.com/800/200/food",
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    src: "https://cf.shopee.vn/file/fa12360747f5d0e510f2a0be28822093_xxhdpi",
+    altText: 'Slide 2'
   },
   {
     id: 3,
-    src: "https://loremflickr.com/800/200/cars",
-    altText: 'Slide 3',
-    caption: 'Slide 3'
+    src: "https://cf.shopee.vn/file/f53405fc6835381e6a003970be6dff2e_xxhdpi",
+    altText: 'Slide 3'
   }
 ];
 
@@ -60,31 +57,39 @@ const Advertisement = (props) => {
         onExited={() => setAnimating(false)}
       >
         <img src={item.src} alt={item.altText} />
-        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
       </CarouselItem>
     );
   });
-
+  const src = 'https://salt.tikicdn.com/cache/w408/ts/banner/aa/3b/b3/403c766e847f8a4509ad21f6cadcb6bb.jpg';
   return (
-    <div style={{width: 800, margin: 50}}>
-      <style>
-        {
-          `.custom-tag {
-              max-width: 100%;
-              height: 500px;
-            }`
-        }
-      </style>
-      <Carousel
-        activeIndex={activeIndex}
-        next={next}
-        previous={previous}
-      >
-        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-        {slides}
-        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-      </Carousel>
+    <div style={{ display: 'flex', marginTop : 50, marginLeft : 100}}>
+      <div style={{width: 800, display: 'inline-block'}}>
+        <style>
+          {
+            `.custom-tag {
+                max-width: 100%;
+              }`
+          }
+        </style>
+        <Carousel
+          activeIndex={activeIndex}
+          next={next}
+          previous={previous}
+        >
+          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+        </Carousel>
+      </div>
+      <div className="advertisement-picture" 
+          style={{display: 'inline-block',
+          width : "408px",
+          height: "270px",
+          marginLeft: "20px",
+          backgroundImage : `url(${src})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover" }} />
     </div>
   );
 }

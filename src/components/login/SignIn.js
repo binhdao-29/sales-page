4139a2c918317fff347214ce46386e889f872bb0
binhdao29 <binhchan0293@gmail.com>
2021-03-21 23:36:37 +0700

    fix bug react-scripts npm start

--- src/components/login/SignIn.js
+++ src/components/login/SignIn.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 
 import './Login.css';
 
-export default function SignIn() {
+export default function SignIn(props) {
+
     return <div className="SignUp">
         <Container>
             <h1 style={{marginBottom: 40}}>Login</h1>
             <Form>
                 <FormGroup>
                     <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="Your email" />
+                    <Input type="email" name="email" 
+                            id="exampleEmail" placeholder="Your email"
+                             />
                 </FormGroup>
                 <FormGroup>
                     <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="Enter your password" />
+                    <Input type="password" name="password" 
+                            id="examplePassword" placeholder="Enter your password"
+                            />
                 </FormGroup>
                 <FormGroup check>
                     <Label check>
@@ -22,7 +27,7 @@ export default function SignIn() {
                         Remember your password?
                     </Label>
                 </FormGroup>
-                <Button>Sign In</Button>
+                <Button type="button">Sign In</Button>
             </Form>
         </Container>
     </div>
